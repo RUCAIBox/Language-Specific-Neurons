fdeca23acd53d7a4de5ca3f7a1b66ef94d7e19a7
Tianyi Tang <37647985+StevenTang1998@users.noreply.github.com>
2024-12-23 15:59:12 +0800

    Update activation.py

--- activation.py
+++ activation.py
@@ -17,10 +17,6 @@ max_length = model.llm_engine.model_config.max_model_len
 num_layers = model.llm_engine.model_config.hf_config.num_hidden_layers
 intermediate_size = model.llm_engine.model_config.hf_config.intermediate_size if is_llama else model.llm_engine.model_config.hf_config.hidden_size * 4
 
-sum1 = torch.zeros(num_layers, intermediate_size).to('cuda')
-sum2 = torch.zeros(num_layers, intermediate_size).to('cuda')
-sum3 = torch.zeros(num_layers, intermediate_size).to('cuda')
-sum4 = torch.zeros(num_layers, intermediate_size).to('cuda')
 over_zero = torch.zeros(num_layers, intermediate_size, dtype=torch.int32).to('cuda')
 
 def factory(idx):
@@ -29,10 +25,6 @@ def factory(idx):
         i = gate_up.size(-1)
         gate_up[:, :, : i // 2] = torch.nn.SiLU()(gate_up[:, :, : i // 2])
         activation = gate_up[:, :, : i // 2].float() # b, l, i
-        sum1[idx, :] += activation.sum(dim=(0,1))
-        sum2[idx, :] += activation.pow(2).sum(dim=(0,1))
-        sum3[idx, :] += activation.pow(3).sum(dim=(0,1))
-        sum4[idx, :] += activation.pow(4).sum(dim=(0,1))
         over_zero[idx, :] += (activation > 0).sum(dim=(0,1))
         x = gate_up[:, :, : i // 2] * gate_up[:, :, i // 2 :]
         x, _ = self.down_proj(x)
@@ -42,10 +34,6 @@ def factory(idx):
         x, _ = self.dense_h_to_4h(x)
         x = self.gelu_impl(x)
         activation = x.float()
-        sum1[idx, :] += activation.sum(dim=(0,1))
-        sum2[idx, :] += activation.pow(2).sum(dim=(0,1))
-        sum3[idx, :] += activation.pow(3).sum(dim=(0,1))
-        sum4[idx, :] += activation.pow(4).sum(dim=(0,1))
         over_zero[idx, :] += (activation > 0).sum(dim=(0,1))
         x, _ = self.dense_4h_to_h(x)
         return x
@@ -73,7 +61,7 @@ input_ids = ids[:l].reshape(-1, max_length)
 
 output = model.generate(prompt_token_ids=input_ids.tolist(), sampling_params=SamplingParams(max_tokens=1))
 
-output = dict(n=l, sum1=sum1.to('cpu'), sum2=sum2.to('cpu'), sum3=sum3.to('cpu'), sum4=sum4.to('cpu'), over_zero=over_zero.to('cpu'))
+output = dict(n=l, over_zero=over_zero.to('cpu'))
 
 if is_llama:
     torch.save(output, f'data/activation.{lang}.train.llama-7b')
